7aeb83ae237b68b35567ebc93e12bbccf91172cb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-12 22:46:46 +0000

    Deploy jsmnbom/mommys-cookbook to github.com/jsmnbom/mommys-cookbook.git:gh-pages

---
+++ precache-manifest.cb3ef91fe03726709a755f263376fd6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92d1f46b596c118c0947",
+    "url": "/mommys-cookbook/css/app.f02e4511.css"
+  },
+  {
+    "revision": "92d1f46b596c118c0947",
+    "url": "/mommys-cookbook/js/app.d91b2a00.js"
+  },
+  {
+    "revision": "d2e1b512903bc54f25a7",
+    "url": "/mommys-cookbook/css/chunk-vendors.f6f60332.css"
+  },
+  {
+    "revision": "d2e1b512903bc54f25a7",
+    "url": "/mommys-cookbook/js/chunk-vendors.289f771e.js"
+  },
+  {
+    "revision": "b7325467e382e7bf26930bc3f786b82d",
+    "url": "/mommys-cookbook/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mommys-cookbook/robots.txt"
+  }
+];
